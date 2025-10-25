e96d60ccd1b9eb75e63f3221ca89eb6753743b84
ThisaraniAloka <164716628+ThisaraniAloka@users.noreply.github.com>
2025-10-25 08:38:47 +0530

    update

--- server/server.js
+++ server/server.js
@@ -3,7 +3,8 @@ import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './configs/db.js';
 import { clerkMiddleware } from '@clerk/express'
-
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js"
 
 const app = express();
 const port = 3000;
@@ -18,5 +19,6 @@ app.use(clerkMiddleware())
 
 
 app.get('/', (req, res) => res.send("Server is Live!"));
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.listen(port, () => console.log(`Server listening at http://localhost:${port}`));
